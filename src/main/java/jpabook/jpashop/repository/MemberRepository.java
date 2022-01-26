163ca39444a4c7482c4c70581ace878ee2b27caa
430lyj <430lyj@gmail.com>
2022-01-27 04:39:08 +0900

    test : 회원가입, 중복회원예외 처리 테스트

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class MemberRepository {
 
     //@PersistenceContext //표준 애노테이션; 스프링이 자동으로 엔티티 매니저를 생성해서 주입을 해줌.
-    private EntityManager em; //스프링부트(스프링 데이터 JPA)가 @Autowired도 이해할 수 있도록 지원을 해줌 -> 따라서 생성자 주입, 롬복 모두 사용 가능
+    private final EntityManager em; //스프링부트(스프링 데이터 JPA)가 @Autowired도 이해할 수 있도록 지원을 해줌 -> 따라서 생성자 주입, 롬복 모두 사용 가능
 
     public void save(Member member){
         em.persist(member); //영속성 컨텍스트에 멤버 엔티티를 넣음 -> 트랜잭션이 커밋 되는 시점에 Insert 쿼리문이 날아감.
@@ -28,8 +28,8 @@ public class MemberRepository {
                 .getResultList();  //JPQL 사용 (SQL과 유사하지만 SQL은 테이블 단위, JPQL은 엔티티 단위)
     }
 
-    public List<Member> findByName(String name){
-        return em.createQuery("selct m from Member m where m.name = :name", Member.class)
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
                 .setParameter("name", name)
                 .getResultList(); // 파라미터 바인딩
     }
---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,57 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class) //JUnit5되면서 사라짐.
+@SpringBootTest  // 이 두 가지가 있어야 스프링에서도 잘되는지 까지 테스트 가능
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
+
+    @Test
+    //@Rollback(false)
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        em.flush(); //DB에 쿼리를 날리는 것까지 볼 수 있음.
+        assertEquals(member, memberRepository.findOne(savedId));
+    } //JPA의 경우 em.persist()한 경우에 바로 쿼리가 날아가는 게 아니고 transaction이 커밋될 때 한꺼번에 날아감
+      // -> 테스트의 transactional 은 커밋이 되지 않으므로 insert문도 날아가지 않음!
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복회원예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //when
+        memberService.join(member1);
+        memberService.join(member2);
+
+        //then
+//        fail("예외");
+    }
+}
