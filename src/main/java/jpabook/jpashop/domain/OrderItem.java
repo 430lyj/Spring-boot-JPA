e395cee7929c7dea0b1c14f0aa2c96b82793ff01
430lyj <430lyj@gmail.com>
2022-01-28 18:25:14 +0900

    feat : 주문 서비스 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,6 +11,7 @@ import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -26,4 +29,32 @@ public class OrderItem {
     private int orderPrice; //주문 가격
     private int count; //주문 수량
 
+//    protected OrderItem() {
+//    } //객체 생성 방법을 createOrderItem 만 가능하도록 막아둠 -> lombok으로 줄일 수 있음 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        //지금은 시스템이 단순해서 Item 내에 price가 있지만, 할인이나 쿠폰 등의 경우로 orderPrice가 다를 경우를 대비해 orderPrice를 별도로 지정.
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+
+    /**
+     * 주문상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
