4f4232c10526cbb5e2595bd4d8ee489dca8df86d
430lyj <430lyj@gmail.com>
2022-02-01 04:33:34 +0900

    feat : 상품 주문, 주문 목록 검색 기능 화면 개발

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Item;
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,14 @@ public class ItemService {
     public void saveItem(Item item){
         itemRepository.save(item);
     }
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity){ //파라미터로 넘어온 준영속 상태의 엔티티
+        Item findItem = itemRepository.findOne(itemId); //같은 엔티티를 조회(영속 상태)
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
+    } //스프링의 변경 감지에 의해서 @Transactional 하면 커밋이 되기 전 쿼리들이 flush 되면서 em.merge 하지 않아도 자동 DB 업데이트
 
     public List<Item> findItem(){
         return itemRepository.findAll();
--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -4,6 +4,7 @@ import jpabook.jpashop.domain.*;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -56,9 +57,9 @@ public class OrderService {
     }
 
     //검색
-/*
+
     public List<Order> findOrders(OrderSearch orderSearch){
-        retun orderRepository.findAll(orderSearch);
+        return orderRepository.findAllByString(orderSearch);
     }
-*/
+
 }
