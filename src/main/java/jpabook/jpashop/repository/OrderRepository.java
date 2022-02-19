fba63ced603f58982e50641c426c866cd1bed941
430lyj <430lyj@gmail.com>
2022-02-19 20:47:59 +0900

    refactor: 엔티티를 DTO로 변환 (fetch join 이용)

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -95,4 +95,17 @@ public class OrderRepository {
                         " join fetch o.delivery d", Order.class //order 가져올 때 한 방에 delivery도 가져오고 싶다
         ).getResultList();
     }
+
+    public List<Order> findAllWithItem(){
+        return em.createQuery(
+                "select distinct o from Order o " + //DB에 distinct 넣은 것과 동일. but distinct는 한 줄(row)이 다 똑같아야만 중복을 제외하는 것이 작동하는데,
+                        // 여기서는 그런 경우가 없어서 작동되지 않음.
+                        " join fetch o.member m " +
+                        " join fetch o.delivery d " +
+                        " join fetch o.orderItems oi " +
+                        " join fetch oi.item i", Order.class)
+//                .setFirstResult(1)  //1대다 fetch join 하는 경우 페이징 접근 방식이 전체를 다 끌어 올린 후 메모리에서 나누는 방식 (lazy loading 아님)
+//                .setMaxResults(100) //따라서 일대다 fetch join은 페이징 하면 메모리 오버 플로우 발생 -> 불가능하다고 보면 됨.
+                .getResultList();
+    }
 }
